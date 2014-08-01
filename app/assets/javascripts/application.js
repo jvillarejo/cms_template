df50744f4c848329c7ee4a8d979bc14e33a416b3
jvillarejo <arzivian87@gmail.com>
2014-08-01 16:39:12 -0300

    integrated bower

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+
+//= require angular/angular.min.js
+
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'static/landing'
+
+  root 'static#landing'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
