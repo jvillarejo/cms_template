0d558260b07d64b282361059459c84f04995a697
jvillarejo <arzivian87@gmail.com>
2014-09-22 21:46:20 -0300

    removed errbit config

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,6 +1,6 @@
 Airbrake.configure do |config|
-  config.api_key = '375a821bc301589a248a89b0b9d10bf7'
-  config.host    = 'errbit.jonvillage.com'
-  config.port    = 80
-  config.secure  = config.port == 443
+  # config.api_key = '375a821bc301589a248a89b0b9d10bf7'
+  # config.host    = 'errbit.jonvillage.com'
+  # config.port    = 80
+  # config.secure  = config.port == 443
 end
